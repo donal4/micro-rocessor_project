dae2cdc2165cfcc8ae1e09d84f2ce986b8d4da4e
donal4 <donalbreen12345@gmail.com>
2025-03-11 13:32:06 +0000

    halffixedstuff

--- src/main.c
+++ src/main.c
@@ -13,6 +13,9 @@ void SysTick_Handler(void);
 void delay(volatile uint32_t dly);
 void setupIO();
 
+//serial 
+void initSerial();
+
 //sound 
 //void playNote(uint32_t Freq);
 //void initSound(void);
@@ -32,7 +35,7 @@ int isInside(uint16_t x1, uint16_t y1, uint16_t w, uint16_t h, uint16_t px, uint
 void enablePullUp(GPIO_TypeDef *Port, uint32_t BitNumber);
 void pinMode(GPIO_TypeDef *Port, uint32_t BitNumber, uint32_t Mode);
 
-void item_gen(hinverted ,randy,randx); 
+void item_gen(int ,int ,int );
 
 volatile uint32_t milliseconds;
 
@@ -123,6 +126,8 @@ int main()
 		hmoved = vmoved = 0;
 		hinverted = vinverted = 0;
 
+		//test serial
+		eputs(hp);
 		//Lilguy code
 		if ((GPIOB->IDR & (1 << 4))==0) // right pressed
 		{					
@@ -237,9 +242,9 @@ int main()
 				score+=1;
 				//play sound 
 				playNote(B6);//coin sound 
-				delay(1);//delay
+				delay(10);//delay
 				playNote(B7);//octave above 
-				delay(1);//delay
+				delay(10);//delay
 				playNote(0);//stops sound
 
 				/*
@@ -507,3 +512,48 @@ void health(){
 	}
 
 }	
+
+//______________________________________________________________________________________________________________
+//serial 
+
+void initSerial()
+{
+	/* On the nucleo board, TX is on PA2 while RX is on PA15 */
+	RCC->AHBENR |= (1 << 17); // enable GPIOA
+	RCC->APB2ENR |= (1 << 14); // enable USART1
+	pinMode(GPIOA,2,2); // enable alternate function on PA2
+	pinMode(GPIOA,15,2); // enable alternate function on PA15
+	// AF1 = USART1 TX on PA2
+	GPIOA->AFR[0] &= 0xfffff0ff;
+	GPIOA->AFR[0] |= (1 << 8);
+	// AF1 = USART1 RX on PA15
+	GPIOA->AFR[1] &= 0x0fffffff;
+	GPIOA->AFR[1] |= (1 << 28);
+	// De-assert reset of USART1 
+	RCC->APB2RSTR &= ~(1u << 14);
+	
+	USART1->CR1 = 0; // disable before configuration
+	USART1->CR3 |= (1 << 12); // disable overrun detection
+	USART1->BRR = 48000000/9600; // assuming 48MHz clock and 9600 bps data rate
+	USART1->CR1 |= (1 << 2) + (1 << 3); // enable Transmistter and receiver
+	USART1->CR1 |= 1; // enable the UART
+
+}
+
+
+//gets charactar 
+char egetchar()
+{
+	while( (USART1->ISR & (1 << 5)) == 0); // wait for a character
+	return (char)USART1->RDR; // return the character that is waiting in the Receive Data Register
+}
+
+//puts string 
+void eputs(char *String)
+{
+	while(*String) // keep printing until a NULL is found
+	{
+		eputchar(*String);
+		String++;
+	}
+}
